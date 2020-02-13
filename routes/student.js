cbf8d304cd6182aed57a47faffc6f225d9db128f
Chanchal Sadhukhan <cksadhukhan@gmail.com>
2020-02-13 16:23:08 +0530

    Route edited

---
+++ routes/student.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+// const department = require('../controllers/department');
+
+// router.post('/create', department.create);
+// router.post('/modify/', department.modify);
+// router.get('/:id', department.read);
+// router.get('/delete', department.delete);
+
+// router.get('/about', (req, res) => {
+// 	res.send('Department about');
+// });
+
+module.exports = router;
